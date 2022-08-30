86a8a09fc7f790eea1fda55e37febe1e5ca42fd4
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2022-08-30 17:36:08 +0100

    Default thing

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
